84a7c3bc48f368ddcbd76927e427408e1e95965e
tala-kha <s12324200@stu.najah.edu>
2025-04-13 12:58:21 +0300

    create dashboard and analysis page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './modules/public/home/home.component';
 import { RegisterComponent } from './modules/auth/register/register.component';
 import { LoginComponent } from './modules/auth/login/login.component';
 import { NotfoundComponent } from './shared/components/notfound/notfound.component';
+import { DashboardComponent } from './modules/job-seeker/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {path: 'home', component:HomeComponent, title:'Home'},
@@ -10,5 +11,10 @@ export const routes: Routes = [
   {path:'register',component:RegisterComponent, title:'Register'},
   {path:'login',component:LoginComponent, title:'Login'},
   {path:'notfound', component:NotfoundComponent, title:'Not Found'},
+  {path: 'dashboard', 
+    loadComponent: () =>
+      import('./modules/job-seeker/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    title: 'Dashboard'
+  },
   {path:'**', redirectTo:'notfound', pathMatch:'full'}
 ];
