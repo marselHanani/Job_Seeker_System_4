9963f390f0366b78a19f7c904caf109e1d941f17
Ash-our <s12324069l@stu.najah.edu>
2025-04-16 13:58:23 +0300

    jobSeekerPages

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  navItems = [
+    { id: 'home', label: 'Home' },
+    { id: 'findJob', label: 'Find Job' },
+    { id: 'findEmployers', label: 'Find Employers' },
+    { id: 'dashboard', label: 'Dashboard' },
+    { id: 'jobAlerts', label: 'Job Alerts' },
+    { id: 'customerSupports', label: 'Customer Supports' }
+  ];
+}
