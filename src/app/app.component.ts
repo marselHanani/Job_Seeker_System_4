5ab1d8b0cc1e2500935dc44f510817078e6f2f5b
Alaa-Affori <alaa.afoore@gmail.com>
2025-04-16 17:45:12 +0300

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,9 @@ import { NotificationsComponent } from './notifications/notifications.component'
 
 @Component({
   selector: 'app-root',
-<<<<<<< HEAD
+
   imports: [RouterOutlet, NavbarComponent, FooterComponent,EditProfileComponent,UploadResumeComponent,ViewProfileComponent,NotificationsComponent],
-=======
-  imports: [RouterOutlet, NavbarComponent, FooterComponent,EditProfileComponent,UploadResumeComponent,ViewProfileComponent, NotificationsComponent],
->>>>>>> bd51f577e4aa852bf27039bbab4c9d1a685a8e16
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
