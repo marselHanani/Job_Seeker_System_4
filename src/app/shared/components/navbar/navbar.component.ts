6d23ebecec20eec53a40344c4e37f2b49179258a
Ash-our <s12324069l@stu.najah.edu>
2025-04-17 05:35:11 +0300

    some edits to css

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -11,7 +11,7 @@ import { RouterModule } from '@angular/router';
 export class NavbarComponent {
   navItems = [
     { id: 'home', label: 'Home' },
-    { id: 'findJob', label: 'Find Job' },
+    { id: 'jobs', label: 'Find Job' },
     { id: 'findEmployers', label: 'Find Employers' },
     { id: 'dashboard', label: 'Dashboard' },
     { id: 'jobAlerts', label: 'Job Alerts' },
