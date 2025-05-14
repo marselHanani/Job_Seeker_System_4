0c10ca3eda9a80f3cf3cbb7602be051a6ae88af4
Alaa-Affori <alaa.afoore@gmail.com>
2025-05-14 13:42:18 +0300

    edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
+import { EditProfileComponent } from './modules/job-seeker/edit-profile/edit-profile.component';
+import { UploadResumeComponent } from './modules/job-seeker/upload-resume/upload-resume.component';
+import { ViewProfileComponent } from './modules/job-seeker/view-profile/view-profile.component';
+import { NotificationsComponent } from './shared/components/notifications/notifications.component';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent, FooterComponent,EditProfileComponent,UploadResumeComponent,ViewProfileComponent, NotificationsComponent],
